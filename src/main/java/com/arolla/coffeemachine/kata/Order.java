99a1d1723f6c93de93e69a39418f8a18f7b1a3da
Ala <ala.dogui.ext@eulerhermes.com>
2021-03-01 19:36:56 +0100

    [First iteration] Create failed tests of order

--- src/main/java/com/arolla/coffeemachine/kata/Order.java
+++ src/main/java/com/arolla/coffeemachine/kata/Order.java
@@ -1,5 +1,48 @@
 package com.arolla.coffeemachine.kata;
 
+import com.arolla.coffeemachine.kata.types.Drink;
+
 public class Order {
+
+  private Drink drinkType;
+  private int numberOfSugar;
+  private boolean withStick;
+
+  public Order(final Drink drinkType) {
+    this.drinkType = drinkType;
+  }
+
+  public Order(final Drink drinkType, final int numberOfSugar) {
+    this.drinkType = drinkType;
+    this.numberOfSugar = numberOfSugar;
+  }
+
+  public boolean hasSugar(){
+    return false;
+  }
+
+  public boolean hasStick(){
+    return false;
+  }
+
+  public void addOneSugar(){
+
+  }
+
+  public void addTwoSugar(){
+
+  }
+
+  public int getNumberOfSugar() {
+    return numberOfSugar;
+  }
+
+  public Drink getDrinkType() {
+    return drinkType;
+  }
+
+  public String toString(){
+    return "";
+  }
 
 }
--- src/test/java/com/arolla/coffeemachine/kata/OrderTest.java
+++ src/test/java/com/arolla/coffeemachine/kata/OrderTest.java
@@ -1,12 +1,59 @@
 package com.arolla.coffeemachine.kata;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.arolla.coffeemachine.kata.types.Coffee;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class OrderTest {
+public class OrderTest {
+
+  private Order order;
 
   @BeforeEach
   void setUp() {
+    order = new Order(new Coffee());
   }
+
+  @Test
+  public void should_add_stick_when_create_order_with_sugar(){
+    order = new Order(new Coffee(), 2);
+    assertTrue(order.hasStick());
+  }
+
+  @Test
+  public void should_return_coffee_instruction_order() {
+    assertEquals("C::", order.toString());
+  }
+
+  @Test
+  public void should_return_hasSugar_true_when_sugar_present() {
+    order.addOneSugar();
+    assertTrue(order.hasSugar());
+  }
+
+  @Test
+  public void should_return_withStick_true_when_sugar_exist() {
+    order.addOneSugar();
+    assertTrue(order.hasStick());
+  }
+
+  @Test
+  public void should_return_current_number_of_sugar_plus_one_when_add_one_sugar() {
+    assertEquals(order.getNumberOfSugar(), 0);
+    order.addOneSugar();
+    assertEquals(order.getNumberOfSugar(), 1);
+  }
+
+  @Test
+  public void should_return_current_number_of_sugar_plus_two_when_add_two_sugar() {
+    assertEquals(order.getNumberOfSugar(), 0);
+    order.addTwoSugar();
+    assertEquals(order.getNumberOfSugar(), 2);
+  }
+
 }
+
+
+
