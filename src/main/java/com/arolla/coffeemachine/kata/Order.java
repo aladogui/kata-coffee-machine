a97c71dbe8eb06c96352bbfe4d77abab709dff57
Ala <ala.dogui.ext@eulerhermes.com>
2021-03-01 19:18:04 +0100

    Init project

---
+++ src/main/java/com/arolla/coffeemachine/kata/Order.java
@@ -0,0 +1,5 @@
+package com.arolla.coffeemachine.kata;
+
+public class Order {
+
+}
---
+++ src/test/java/com/arolla/coffeemachine/kata/OrderTest.java
@@ -0,0 +1,12 @@
+package com.arolla.coffeemachine.kata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class OrderTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+}
